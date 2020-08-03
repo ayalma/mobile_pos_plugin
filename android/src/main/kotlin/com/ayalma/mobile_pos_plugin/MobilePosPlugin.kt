2cbfe973ec2c3fca0546624bb850c51c508a2e7b
Ali Mohammadi <alimohammadi7117@gmail.com>
2020-08-03 16:33:08 +0430

    fixed bug

--- android/src/main/kotlin/com/ayalma/mobile_pos_plugin/MobilePosPlugin.kt
+++ android/src/main/kotlin/com/ayalma/mobile_pos_plugin/MobilePosPlugin.kt
@@ -145,7 +145,7 @@ public class MobilePosPlugin : FlutterPlugin, MethodCallHandler, ActivityAware{
         var invoiceNumber = args[0]
         var amount = args[1]
 
-        posSdk?.purchase(amount,invoiceNumber) { type: PurchaseResultType, data: List<Any?> ->
+        posSdk?.purchase(amount,invoiceNumber) { type: PurchaseResultType, data: Map<String, Any?> ->
             val methodName = when(type){
                 PurchaseResultType.InitializationFailed->PURCHASE_ON_PAYMENT_INITIALIZATION_FAILED
                 PurchaseResultType.Cancelled->PURCHASE_ON_PAYMENT_CANCELLED
